f62c1a2d346a88312d4d16125854e1fb7ac1bfa3
Brad Daily <brad@slideshowpro.net>
2012-10-19 12:44:48 -0500

    Initial commit

---
+++ koken-google-analytics/plugin.php
@@ -0,0 +1,30 @@
+<?php
+
+class KokenGoogleAnalytics extends KokenPlugin {
+
+	function __construct()
+	{
+		$this->register_output_filter('before_closing_head', 'render');
+	}
+
+	function render()
+	{
+
+		return <<<OUT
+<script type="text/javascript">
+
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', '{$this->data->tracking_id}']);
+ 	_gaq.push(['_trackPageview']);
+
+ 	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+ 	})();
+
+</script>
+OUT;
+
+	}
+}
